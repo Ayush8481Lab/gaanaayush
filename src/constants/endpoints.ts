bf0b67e59e2c7d0707b33362673c88be7dd41ae2
Ayush8481Lab <wonder945177@gmail.com>
2026-05-07 01:35:43 +0530

    Update endpoints.ts

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -13,7 +13,7 @@
 export const apiEndpoints = {
   trendingUrl: 'https://gaana.com/apiv2?type=miscTrendingSongs',
   albumListUrl: 'https://gaana.com/apiv2?type=albumList',
-  searchSongsUrl: 'https://gsearch.gaana.com/vichitih/go/v2/?geoLocation=GLOBAL&content_filter=2&include=album&isRegSrch=0&webVersion=mix&rType=web&startIndex=0&usrLang=Bhojpuri&query=',
+  searchSongsUrl: 'https://gaana.com/apiv2?country=IN&page=0&secType=track&type=search&usrLang=Bhojpuri&keyword=',
   songDetailsUrl: 'https://gaana.com/apiv2?type=songDetail&seokey=',
   similarArtistsUrl: 'https://apiv2.gaana.com/player/similar-artists/',
   playlistDetailsUrl: 'https://gaana.com/apiv2?type=playlistDetail&seokey=',
