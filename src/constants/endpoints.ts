808744732a894e4b5e0b6ad0f09b02cdfac27965
Ayush8481Lab <wonder945177@gmail.com>
2026-05-07 10:20:51 +0530

    Update endpoints.ts

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -13,7 +13,7 @@
 export const apiEndpoints = {
   trendingUrl: 'https://gaana.com/apiv2?type=miscTrendingSongs',
   albumListUrl: 'https://gaana.com/apiv2?type=albumList',
-  searchSongsUrl: 'https://gaana.com/apiv2?country=IN&page=0&secType=track&type=search&limit=2&keyword=',
+  searchSongsUrl: 'https://gaana.com/apiv2?country=IN&page=0&secType=track&type=search&keyword=',
   songDetailsUrl: 'https://gaana.com/apiv2?type=songDetail&seokey=',
   similarArtistsUrl: 'https://apiv2.gaana.com/player/similar-artists/',
   playlistDetailsUrl: 'https://gaana.com/apiv2?type=playlistDetail&seokey=',
