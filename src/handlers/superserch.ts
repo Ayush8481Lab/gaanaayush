9a626102fd7c75e03ac2dd7a79c8c1e4d57c7c15
Ayush8481Lab <wonder945177@gmail.com>
2026-05-09 10:28:02 +0530

    Update superserch.ts

--- src/handlers/superserch.ts
+++ src/handlers/superserch.ts
@@ -16,22 +16,18 @@ export const handleSuperserch = async (c: Context) => {
   try {
     // 2. Reconstruct the URL. 
     // If endpointPath is empty, this perfectly builds "https://apiv2.gaana.com/?query=..."
-    const targetUrl = `https://gsearch.gaana.com/${endpointPath}${reqUrl.search}`
+    const targetUrl = `https://apiv2.gaana.com/${endpointPath}${reqUrl.search}`
 
     // 3. Fetch from Gaana using our bypass headers
     const response = await fetch(targetUrl, {
       method: 'GET',
       headers: {
-  "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
-        "referer": "https://gaana.com/search/songs/hellllll",
-        "accept": "text/css,*/*;q=0.1",
-        "accept-encoding": "gzip, deflate, br, zstd",
-        "connection": "keep-alive",
-        "cookie": "deviceId=4aa04b60-4bb4-4903-9bc0-7db7dc91c41a; deviceType=GaanaWebsiteApp; __ul=Hindi%2CEnglish",
-        "host": "gaana.com",
-        "sec-fetch-dest": "style",
-        "sec-fetch-mode": "no-cors",
-        "sec-fetch-site": "same-origin"
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+        'deviceType': 'GaanaAndroidApp',
+        'appVersion': 'V5',
+        'Accept': 'application/json, text/plain, */*',
+        'Origin': 'https://gaana.com',
+        'Referer': 'https://gaana.com/'
       }
     })
 
